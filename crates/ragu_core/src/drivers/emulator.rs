89a0c109d051504e3a8385fc10cd13814c4f5175
Sean Bowe <ewillbefull@gmail.com>
2025-11-13 17:25:39 -0700

    emulator: more modular implementation

--- crates/ragu_core/src/drivers/emulator.rs
+++ crates/ragu_core/src/drivers/emulator.rs
@@ -5,36 +5,173 @@ use ff::Field;
 
 use crate::{
     Result,
-    drivers::{Coeff, Driver, DriverTypes, FromDriver},
+    drivers::{Coeff, DirectSum, Driver, DriverTypes, FromDriver, LinearExpression},
     gadgets::GadgetKind,
-    maybe::{Always, MaybeKind},
+    maybe::{Always, Maybe, MaybeKind},
     routines::{Prediction, Routine},
 };
+
+/// Mode that an emulator may be running in; usually either [`Wired`] or
+/// [`Wireless`].
+pub trait Mode {
+    /// The resulting [`Emulator`]'s [`DriverTypes::MaybeKind`].
+    type MaybeKind: MaybeKind;
+
+    /// The resulting [`Emulator`]'s [`DriverTypes::ImplField`].
+    type F: Field;
+
+    /// The resulting [`Emulator`]'s [`DriverTypes::ImplWire`].
+    type Wire: Clone;
+
+    /// The resulting [`Emulator`]'s [`DriverTypes::LCadd`].
+    type LCadd: LinearExpression<Self::Wire, Self::F>;
+
+    /// The resulting [`Emulator`]'s [`DriverTypes::LCenforce`].
+    type LCenforce: LinearExpression<Self::Wire, Self::F>;
+}
+
+/// Mode for an [`Emulator`] that tracks wire assignments.
+pub struct Wired<M: MaybeKind, F: Field>(PhantomData<(M, F)>);
+
+/// Container for a [`Field`] element representing a wire assignment that may or
+/// may not be known depending on the parameterized [`MaybeKind`].
+pub enum MaybeWired<M: MaybeKind, F: Field> {
+    /// The special wire representing the constant one.
+    One,
+
+    /// A wire with an arbitrary assignment.
+    Arbitrary(M::Rebind<F>),
+}
+
+impl<F: Field> MaybeWired<Always<()>, F> {
+    /// Retrieves the underlying wire value.
+    pub fn value(self) -> F {
+        match self {
+            MaybeWired::One => F::ONE,
+            MaybeWired::Arbitrary(value) => value.take(),
+        }
+    }
+}
+
+impl<M: MaybeKind, F: Field> Clone for MaybeWired<M, F> {
+    fn clone(&self) -> Self {
+        match self {
+            MaybeWired::One => MaybeWired::One,
+            MaybeWired::Arbitrary(value) => MaybeWired::Arbitrary(value.clone()),
+        }
+    }
+}
+
+/// Implementation of [`LinearExpression`] for a [`DirectSum`] that may or may
+/// not have a known value depending on the parameterized [`MaybeKind`].
+pub struct MaybeDirectSum<M: MaybeKind, F: Field>(M::Rebind<DirectSum<F>>);
+
+impl<M: MaybeKind, F: Field> LinearExpression<MaybeWired<M, F>, F> for MaybeDirectSum<M, F> {
+    fn add_term(self, wire: &MaybeWired<M, F>, coeff: Coeff<F>) -> Self {
+        MaybeDirectSum(self.0.map(|sum| {
+            let wire = match wire {
+                MaybeWired::One => &F::ONE,
+                MaybeWired::Arbitrary(wire) => wire.snag(),
+            };
+            sum.add_term(wire, coeff)
+        }))
+    }
+
+    fn gain(self, coeff: Coeff<F>) -> Self {
+        MaybeDirectSum(self.0.map(|sum| sum.gain(coeff)))
+    }
+
+    fn extend(self, with: impl IntoIterator<Item = (MaybeWired<M, F>, Coeff<F>)>) -> Self {
+        MaybeDirectSum(self.0.map(|sum| {
+            sum.extend(with.into_iter().map(|(wire, coeff)| {
+                let wire = match wire {
+                    MaybeWired::One => F::ONE,
+                    MaybeWired::Arbitrary(wire) => wire.take(),
+                };
+                (wire, coeff)
+            }))
+        }))
+    }
+
+    fn add(self, wire: &MaybeWired<M, F>) -> Self {
+        MaybeDirectSum(self.0.map(|sum| {
+            let wire = match wire {
+                MaybeWired::One => &F::ONE,
+                MaybeWired::Arbitrary(wire) => wire.snag(),
+            };
+            sum.add(wire)
+        }))
+    }
+
+    fn sub(self, wire: &MaybeWired<M, F>) -> Self {
+        MaybeDirectSum(self.0.map(|sum| {
+            let wire = match wire {
+                MaybeWired::One => &F::ONE,
+                MaybeWired::Arbitrary(wire) => wire.snag(),
+            };
+            sum.sub(wire)
+        }))
+    }
+}
+
+impl<M: MaybeKind, F: Field> Mode for Wired<M, F> {
+    type MaybeKind = M;
+    type F = F;
+    type Wire = MaybeWired<M, F>;
+    type LCadd = MaybeDirectSum<M, F>;
+    type LCenforce = MaybeDirectSum<M, F>;
+}
+
+/// Mode for an [`Emulator`] that does not track wire assignments.
+pub struct Wireless<M: MaybeKind, F: Field>(PhantomData<(M, F)>);
+
+impl<M: MaybeKind, F: Field> Mode for Wireless<M, F> {
+    type MaybeKind = M;
+    type F = F;
+    type Wire = ();
+    type LCadd = ();
+    type LCenforce = ();
+}
 
 /// A driver used to execute circuit synthesis code and obtain the result of a
 /// computation without enforcing constraints or collecting a witness. Useful
 /// for obtaining the result of a computation that is later executed with
 /// another driver.
-pub struct Emulator<M: MaybeKind, F: Field> {
-    _marker: PhantomData<(M, F)>,
-}
+pub struct Emulator<M: Mode>(PhantomData<M>);
 
-impl<F: Field> Emulator<Always<()>, F> {
-    /// Create an emulator that is used to simply execute, rather than
-    /// collecting wire assignments.
+impl<F: Field> Emulator<Wireless<Always<()>, F>> {
+    /// Creates a new `Emulator` driver in wireless mode, specifically for
+    /// executing with a known witness.
     pub fn execute() -> Self {
         Self::wireless()
     }
 }
 
-impl<M: MaybeKind, F: Field> Emulator<M, F> {
-    /// Creates a new `Emulator` driver.
+impl<M: MaybeKind, F: Field> Emulator<Wireless<M, F>> {
+    /// Creates a new `Emulator` driver in wireless mode, parameterized on the
+    /// existence of a witness.
     pub fn wireless() -> Self {
-        Emulator {
-            _marker: PhantomData,
-        }
+        Emulator(PhantomData)
     }
+}
 
+impl<M: MaybeKind, F: Field> Emulator<Wired<M, F>> {
+    /// Creates a new `Emulator` while tracking wire assignments, parameterized
+    /// on the existence of a witness.
+    pub fn simulator() -> Self {
+        Emulator(PhantomData)
+    }
+}
+
+impl<F: Field> Emulator<Wired<Always<()>, F>> {
+    /// Creates a new `Emulator` while tracking wire assignments, specifically
+    /// for extracting the wire values afterward.
+    pub fn extractor() -> Self {
+        Emulator(PhantomData)
+    }
+}
+
+impl<M: Mode<F = F>, F: Field> Emulator<M> {
     /// Executes a closure with this driver, returning its output.
     pub fn just<R, W: Send>(&mut self, f: impl FnOnce(&mut Self) -> Result<R>) -> Result<R> {
         f(self)
@@ -45,21 +182,21 @@ impl<M: MaybeKind, F: Field> Emulator<M, F> {
     pub fn with<R, W: Send>(
         &mut self,
         witness: W,
-        f: impl FnOnce(&mut Self, M::Rebind<W>) -> Result<R>,
+        f: impl FnOnce(&mut Self, <M::MaybeKind as MaybeKind>::Rebind<W>) -> Result<R>,
     ) -> Result<R> {
-        f(self, M::maybe_just(|| witness))
+        f(self, M::MaybeKind::maybe_just(|| witness))
     }
 }
 
-impl<M: MaybeKind, F: Field> DriverTypes for Emulator<M, F> {
-    type ImplField = F;
-    type ImplWire = ();
-    type MaybeKind = M;
-    type LCadd = ();
-    type LCenforce = ();
+impl<M: Mode> DriverTypes for Emulator<M> {
+    type ImplField = M::F;
+    type ImplWire = M::Wire;
+    type MaybeKind = M::MaybeKind;
+    type LCadd = M::LCadd;
+    type LCenforce = M::LCenforce;
 }
 
-impl<'dr, M: MaybeKind, F: Field> Driver<'dr> for Emulator<M, F> {
+impl<'dr, M: MaybeKind, F: Field> Driver<'dr> for Emulator<Wireless<M, F>> {
     type F = F;
     type Wire = ();
     const ONE: Self::Wire = ();
@@ -97,7 +234,62 @@ impl<'dr, M: MaybeKind, F: Field> Driver<'dr> for Emulator<M, F> {
     }
 }
 
-impl<'dr, D: Driver<'dr>> FromDriver<'dr, '_, D> for Emulator<D::MaybeKind, D::F> {
+impl<'dr, M: MaybeKind, F: Field> Driver<'dr> for Emulator<Wired<M, F>> {
+    type F = F;
+    type Wire = MaybeWired<M, F>;
+    const ONE: Self::Wire = MaybeWired::One;
+
+    fn alloc(&mut self, f: impl Fn() -> Result<Coeff<Self::F>>) -> Result<Self::Wire> {
+        f().map(|coeff| MaybeWired::Arbitrary(M::maybe_just(|| coeff.value())))
+    }
+
+    fn constant(&mut self, coeff: Coeff<Self::F>) -> Self::Wire {
+        MaybeWired::Arbitrary(M::maybe_just(|| coeff.value()))
+    }
+
+    fn mul(
+        &mut self,
+        f: impl Fn() -> Result<(Coeff<Self::F>, Coeff<Self::F>, Coeff<Self::F>)>,
+    ) -> Result<(Self::Wire, Self::Wire, Self::Wire)> {
+        let (a, b, c) = f()?;
+
+        // Despite wires existing, the emulator does not enforce multiplication
+        // constraints.
+
+        Ok((
+            MaybeWired::Arbitrary(M::maybe_just(|| a.value())),
+            MaybeWired::Arbitrary(M::maybe_just(|| b.value())),
+            MaybeWired::Arbitrary(M::maybe_just(|| c.value())),
+        ))
+    }
+
+    fn add(&mut self, lc: impl Fn(Self::LCadd) -> Self::LCadd) -> Self::Wire {
+        let lc = lc(MaybeDirectSum(M::maybe_just(|| DirectSum::default())));
+        MaybeWired::Arbitrary(lc.0.map(|sum| sum.value))
+    }
+
+    fn enforce_zero(&mut self, _: impl Fn(Self::LCenforce) -> Self::LCenforce) -> Result<()> {
+        // Despite wires existing, the emulator does not enforce linear
+        // constraints.
+
+        Ok(())
+    }
+
+    fn routine<R: Routine<Self::F> + 'dr>(
+        &mut self,
+        routine: R,
+        input: <R::Input as GadgetKind<Self::F>>::Rebind<'dr, Self>,
+    ) -> Result<<R::Output as GadgetKind<Self::F>>::Rebind<'dr, Self>> {
+        // Emulator will short-circuit execution if the routine can predict its
+        // output, as the emulator is not involved in enforcing any constraints.
+        match routine.predict(self, &input)? {
+            Prediction::Known(output, _) => Ok(output),
+            Prediction::Unknown(aux) => routine.execute(self, input, aux),
+        }
+    }
+}
+
+impl<'dr, D: Driver<'dr>> FromDriver<'dr, '_, D> for Emulator<Wireless<D::MaybeKind, D::F>> {
     type NewDriver = Self;
 
     fn convert_wire(&mut self, _: &D::Wire) -> Result<()> {
